6ff3530d3e090344dd44f9e01468f0cb77902019
anasauce <anasauceda1843@gmail.com>
2016-12-20 17:20:12 -0800

    refactored to remove unused code

--- src/components/account/details/UpdateToggle.js
+++ src/components/account/details/UpdateToggle.js
@@ -3,33 +3,11 @@ import React, { Component } from 'react'
 import '../../../styles/account/details/update_toggle.css'
 
 export default class UpdateToggle extends Component {
-  constructor( props ) {
-    super( props )
-
-    this.state = {
-      active: false
-    }
-  }
-
-  handleClick () {
-    var active = !this.state.isActive;
-    this.setState({ isActive: active });
-  }
-
   render() {
-    if ( ! this.props.active ) {
-      return (
-        <div className="edit-details__button" onClick={this.handleClick.bind(this)}>
-          <span>{ this.props.name }</span>
-        </div>
-      )
-    }
-    else {
-      return (
-        <div className="edit-details__button">
-          <span>Save Changes</span>
-        </div>
-      )
-    }
+    return (
+      <div className="edit-details__button">
+        <span>{ this.props.name }</span>
+      </div>
+    )
   }
 }
